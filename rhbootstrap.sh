serhepopovych/rhbootstrap

commit bbdcd1bd5ae66464a4095f0ce71c6221ce99ca42

rhbootstrap: Enable nfs_root and non-minimal installs for Fedora
Some (most) versions of Fedora can now share conf.d/all profiles with
CentOS and can produce mostly (fully?) predictable install results.

There is no sense in keeping disabled nfs_root and non-minimal install.

Note that there is no guarantee that installed system will be usable
(e.g. in case of nfs_root) or contain all packages enabled in profile
configuration file.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -2530,12 +2530,6 @@ distro_fedora()
         fi
 
         # On old/new Fedora we do
-
-        # ... not support nfs-root
-        nfs_root=''
-
-        # ... support only minimal install
-        minimal_install=1
     }
 
     local host subdir url
@@ -2775,7 +2769,9 @@ fi
 # $readonly_root
 
 if [ -n "$readonly_root" ]; then
-    if centos_version_gt $releasemaj 7; then
+    if centos_version_gt $releasemaj 7 ||
+       fedora_version_gt $releasemaj 28
+    then
         in_chroot "$install_root" 'yum -y install readonly-root'
     fi
 

commit 75c1e65c450a60705554e49a2f6ee64abc5cd713

rhbootstrap: Disable nfs_root for Fedora < 12 due to missing dracut
Fixes: bbdcd1bd5ae6 ("rhbootstrap: Enable nfs_root and non-minimal installs for Fedora")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -2565,6 +2565,9 @@ distro_fedora()
         fi
 
         # On old/new Fedora we do
+
+        # ... not support nfs-root for Fedora < 12
+        [ $releasemaj -ge 12 ] || nfs_root=''
     }
 
     local host subdir url