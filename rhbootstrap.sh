serhepopovych/rhbootstrap

commit 521657fbf76031ec595789d9db6187c80bbd0ac7

rhbootstrap: Handle GRUB2 EFI configuration file path correctly
On EFI systems GRUB2 configuration file is /etc/grub2-efi.cfg compared
to legacy BIOS systems where GRUB2 configuration file is /etc/grub2.cfg.

On the other hand there might be grub2-efi and grub2-pc installed and
thus both /etc/grub2.cfg and /etc/grub2-efi.cfg present and therefore
both configuration files should be updated.

To address this invoke grub2-mkconfig(1) for each present symlink to
corresponding GRUB2 configuration file.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -1744,8 +1744,12 @@ _EOF
 
         # Update GRUB configuration file
         if [ -n "${pkg_grub2-}" ]; then
-            in_chroot "$install_root" \
-                'grub2-mkconfig -o "$(readlink /etc/grub2.cfg)"'
+            in_chroot "$install_root" '
+                 [ ! -L /etc/grub2.cfg ] ||
+                    grub2-mkconfig -o "$(readlink /etc/grub2.cfg)"
+                 [ ! -L /etc/grub2-efi.cfg ] ||
+                    grub2-mkconfig -o "$(readlink /etc/grub2-efi.cfg)"
+            '
         fi
 
         if [ -n "$nodocs" ]; then

commit 0ddc043b739f553c30a83f835bf42c2e123cd186

rhbootstrap: Use absolute path to GRUB2 config file when readlink
Otherwise we might end with path relative to the current working
directory (pwd) pointing to wrong location for GRUB2 config file.

Fixes: 521657fbf760 ("rhbootstrap: Handle GRUB2 EFI configuration file path correctly")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -2040,9 +2040,9 @@ _EOF
         if [ -n "${pkg_grub2-}" ]; then
             in_chroot "$install_root" '
                  [ ! -L /etc/grub2.cfg ] ||
-                    grub2-mkconfig -o "$(readlink /etc/grub2.cfg)"
+                    grub2-mkconfig -o "$(readlink -f /etc/grub2.cfg)"
                  [ ! -L /etc/grub2-efi.cfg ] ||
-                    grub2-mkconfig -o "$(readlink /etc/grub2-efi.cfg)"
+                    grub2-mkconfig -o "$(readlink -f /etc/grub2-efi.cfg)"
             '
         fi
 