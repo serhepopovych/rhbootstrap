serhepopovych/rhbootstrap

commit f78eb4101918127355d24198d0653ffe4545957b

rhbootstrap: Set $releasever to installed version of CentOS
This is needed to correctly match CentOS versions in case of current
version (e.g. 8) is given by user instead of specific one (e.g. 8.1).

Original $releasever value supplied on command line is stored in
$releasever_orig.

While there make CentOS specific $releasemin variable local and drop
it from Fedora specific setup.

Fixes: 7fae45ec3725 ("rhbootstrap: Introduce and use version comparison helpers")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -2204,6 +2204,18 @@ distro_centos()
     # Usage: distro_post_core_hook
     distro_post_core_hook()
     {
+        local releasemin
+
+        # Determine actually installed version (e.g. 8 -> 8.3)
+        releasever="$(
+            sed -n \
+                -e '1 s/^CentOS\s\+.\+\s\+\([0-9]\+\.[0-9]\+\).*$/\1/p' \
+                '/etc/centos-release' \
+                #
+        )"
+        releasemaj="${releasever%.*}"
+        releasemin="${releasever#*.}"
+
         if [ -n "$is_archive" ]; then
             # Releases available at $baseurl
             local url="${baseurl%/$releasever/*}"
@@ -2451,7 +2463,7 @@ distro_centos()
     }
 
     local host subdir url
-    local _releasemin=255
+    local _releasemin=255 releasemin
 
     # $releasever
     releasever_orig="$releasever"
@@ -2471,7 +2483,6 @@ distro_centos()
 
         [ $releasemaj -ge 4 ] ||
             fatal 'no support for CentOS before 4 (no yum?)'
-
 
         if [ "$releasemaj" != "$releasever" ]; then
             releasemin="${releasever#$releasemaj.}"
@@ -2600,7 +2611,6 @@ distro_fedora()
     }
 
     local host subdir url
-    local _releasemin=255
 
     # $releasever
     releasever_orig="$releasever"
@@ -2614,7 +2624,6 @@ distro_fedora()
         [ $releasemaj -ge 10 ] ||
             fatal 'no support for Fedora before 10 (Fedora Core?)'
     fi
-    releasemin=${_releasemin}
 
     # $subdir
     subdir='fedora/linux'
@@ -6854,7 +6863,7 @@ if [ -n "${has_de-}" ]; then
             PKGS="$PKGS libreoffice-wiki-publisher"
 
         if [ -n "${gtk_based_de-}" ]; then
-            if centos_version_ge $releasever 7.4 ||
+            if centos_version_ge $releasever 7.4 &&
                centos_version_lt $releasever 8.3
             then
                 # libreoffice-gtk2

commit bd66dcb96cbb92e0f0451b1f489ecc71c78ad543

rhbootstrap: Use /etc/redhat-release inside $install_root
Old CentOS release might not have /etc/centos-release file, but both old
and new releases have /etc/redhat-release. Also prefix path to this file
with $install_root to actually read installed system file.

Fixes: f78eb4101918 ("rhbootstrap: Set $releasever to installed version of CentOS")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -2338,7 +2338,7 @@ distro_centos()
         releasever="$(
             sed -n \
                 -e '1 s/^CentOS\s\+.\+\s\+\([0-9]\+\.[0-9]\+\).*$/\1/p' \
-                '/etc/centos-release' \
+                "$install_root/etc/redhat-release" \
                 #
         )"
         releasemaj="${releasever%.*}"