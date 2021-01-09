serhepopovych/rhbootstrap

commit e8b707224dd58908f32239448ca1f87ddcacc4a2

rhbootstrap: Use correct package name for Linux Kernel firmware
In Fedora <= 12 and CentOS < 7 (especially 6.x release) package name
providing firmware blobs is kernel-firmware; while in Fedora > 12
and CentOS >= 7 this package name is linux-firmware.

Use version check helpers to find and add correct firmware package name
to PKGS set. While there drop unnecessary --exclude='linux-firmware'
from @core installation since commit 26d9fd24c429 ("rhbootstrap: Add
support for CentOS releases >= 4.0 and i386 architecture") more generic
pattern --exclude='*firmware*' was introduced.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -2688,7 +2688,7 @@ yum -y \
     --exclude='*firmware*' \
     --exclude='alsa*' \
     --exclude='libertas*' \
-    --exclude='microcode_ctl' --exclude='linux-firmware' \
+    --exclude='microcode_ctl' \
     --exclude='grub2-pc*' --exclude='grub2-efi*' \
     --exclude='NetworkManager*' \
     --exclude='teamd' \
@@ -3143,7 +3143,13 @@ fi
 
 pkg_switch kernel
 if [ -n "${pkg_kernel-}" ]; then
-    PKGS="$PKGS kernel microcode_ctl linux-firmware"
+    PKGS="$PKGS kernel microcode_ctl"
+
+    if fedora_version_le 12 || centos_version_lt 7; then
+        PKGS="$PKGS kernel-firmware"
+    else
+        PKGS="$PKGS linux-firmware"
+    fi
 
     ## /usr/lib/kernel/install.d/zz-symlink-to-root.install
     f="$install_root/usr/lib/kernel/install.d"

commit b59b6c85efdb280f2e27f954a1e175a8a1eb3dd0

rhbootstrap: Fix versioning helpers usage
Fixes: e8b707224dd5 ("rhbootstrap: Use correct package name for Linux Kernel firmware")
Fixes: 69cab9bc20bc ("rhbootstrap: Select correct version of keepass for CentOS and Fedora")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -6168,7 +6168,9 @@ pkg_switch kernel
 if [ -n "${pkg_kernel-}" ]; then
     PKGS="$PKGS kernel microcode_ctl"
 
-    if fedora_version_le 12 || centos_version_lt 7; then
+    if centos_version_lt $releasemaj 7 ||
+       fedora_version_le $releasemaj 12
+    then
         PKGS="$PKGS kernel-firmware"
     else
         PKGS="$PKGS linux-firmware"
@@ -6959,7 +6961,7 @@ if [ -n "${has_de-}" ]; then
         if [ -n "${pkg_keepassx2-}" ]; then
             if centos_version_le $releasemaj 7; then
                 PKGS="$PKGS keepassx2"
-            elif is_centos || fedora_version_ge 26; then
+            elif is_centos || fedora_version_ge $releasemaj 26; then
                 PKGS="$PKGS keepassxc"
             else
                 PKGS="$PKGS keepassx"