serhepopovych/rhbootstrap

commit 1a617f2593d4cde88d2e858ccddbfda1d1ed1bf2

rhbootstrap: Automatically pick repos on host to use for initial setup
This should (however not tested yet) enable Fedora for use as bootstrap
host system.

Fixes: c62c9586c219 ("rhbootstrap: Add support for Fedora >= 10 minimal installation")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -2361,6 +2361,17 @@ if [ -n "$has_glibc_langpack" ]; then
     PKGS="${PKGS:+$PKGS }$f" && unset f
 fi
 
+# Pick repo names on host to configure and use for initial setup
+eval $(
+    yum --noplugins -C repolist | \
+    sed -n -e '2,$ s,^\W\?\(\w\+\).*$,\1,p' | \
+    sed -n -e '1 s,.\+,baserepo=\0,p' \
+           -e '2 s,.\+,updatesrepo=\0,p'
+)
+[ -n "${baserepo-}" ] || baseurl=''
+[ -n "${updatesrepo-}" ] || updatesurl=''
+
+# Initial setup
 setarch "$basearch" \
 yum -y \
     ${releasemaj:+
@@ -2369,13 +2380,13 @@ yum -y \
     ${baseurl:+
         --nogpgcheck
         --disablerepo='*'
-        --enablerepo='fasttrack'
-        --setopt='fasttrack.mirrorlist=file:///dev/null'
-        --setopt="fasttrack.baseurl=$baseurl"
+        --enablerepo="$baserepo"
+        --setopt="$baserepo.mirrorlist=file:///dev/null"
+        --setopt="$baserepo.baseurl=$baseurl"
         ${updatesurl:+
-            --enablerepo='cr'
-            --setopt='cr.mirrorlist=file:///dev/null'
-            --setopt="cr.baseurl=$updatesurl"
+            --enablerepo="$updatesrepo"
+            --setopt="$updatesrepo.mirrorlist=file:///dev/null"
+            --setopt="$updatesrepo.baseurl=$updatesurl"
          }
      } \
     ${install_langs:+
@@ -2428,7 +2439,7 @@ if [ -n "${install_root%/}" ]; then
     }
 fi
 
-# Perform distro specific actions
+# Perform distro specific actions post initial setup
 distro_post_core_hook
 
 # $nfs_root

commit ebeacffa8918f39ed8c517fee9b186e62575dc2e

rhbootstrap: Do not override repo.mirrorlist= with file:///dev/null
It is tested with CentOS 7/8 that repo.baseurl= takes precedence over
repo.mirrorlist= at least when given on command line with --setopt=....
Specifying --setopt=repo.mirrorlist= does not work on CentOS 7.

On the other hand, when specified, may cause failures on Fedora.
Remove for now to fix installation on Fedora.

Fixes: 1a617f2593d4 ("rhbootstrap: Automatically pick repos on host to use for initial setup")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -5763,11 +5763,9 @@ yum -y \
     ${baseurl:+
         --disablerepo='*'
         --enablerepo="$baserepo"
-        --setopt="$baserepo.mirrorlist=file:///dev/null"
         --setopt="$baserepo.baseurl=$baseurl"
         ${updatesurl:+
             --enablerepo="$updatesrepo"
-            --setopt="$updatesrepo.mirrorlist=file:///dev/null"
             --setopt="$updatesrepo.baseurl=$updatesurl"
          }
      } \