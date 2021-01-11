serhepopovych/rhbootstrap

commit fc0fd36fc514f40ad47d58d610232ba72f3c6463

rhbootstrap: Include year and month components of CentOS 7 and 8 release
Otherwise autodetected release from host system will be incorrect and/or
repositories configured to point to non-existent URL.

Introduce and use with centos_version_xx() macros new, CentOS specific,
variable $releasemm that is build from $releasemaj.$releasemin.

Fixes: 91b6fa418218 ("rhbootstrap: Autodetect distro and release from /etc/redhat-release")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -476,7 +476,7 @@ distro_version()
     local root="${1-}"
 
     sed -n \
-        -e '1 s/^CentOS\s\+.\+\s\+\([0-9]\+\.[0-9]\+\).*$/\1/p' \
+        -e '1 s/^CentOS\s\+.\+\s\+\([0-9.]\+\).*$/\1/p' \
         -e '1 s/^Fedora\s\+.\+\s\+\([0-9]\+\).*$/\1/p' \
         "$root/etc/redhat-release" \
         #
@@ -5208,6 +5208,7 @@ distro_centos()
         )"
         releasemaj="${releasever%.*}"
         releasemin="${releasever#*.}"
+        releasemm="$releasemaj.$releasemin"
 
         if [ -n "$is_archive" ]; then
             # Releases available at $baseurl
@@ -5590,6 +5591,8 @@ distro_fedora()
     # Usage: distro_post_core_hook
     distro_post_core_hook()
     {
+        releasemm="$releasever"
+
         if [ -n "$is_archive" ]; then
             # Releases available at $baseurl
             local url="${baseurl%/*/$releasever/*}"
@@ -6882,15 +6885,15 @@ if [ -n "${has_de-}" ]; then
             PKGS="$PKGS libreoffice-wiki-publisher"
 
         if [ -n "${gtk_based_de-}" ]; then
-            if centos_version_ge $releasever 7.4 &&
-               centos_version_lt $releasever 8.3
+            if centos_version_ge $releasemm 7.4 &&
+               centos_version_lt $releasemm 8.3
             then
                 # libreoffice-gtk2
                 pkg_enable libreoffice_gtk2
                 [ -z "${pkg_libreoffice_gtk2-}" ] ||
                     PKGS="$PKGS libreoffice-gtk2"
             fi
-            if centos_version_ge $releasever 7.4; then
+            if centos_version_ge $releasemm 7.4; then
                 # libreoffice-gtk3
                 pkg_enable libreoffice_gtk3
                 [ -z "${pkg_libreoffice_gtk3-}" ] ||

commit a217ac5cc6fa8e95a83c6ffe4191474f4a4999b2

rhbootstrap: Fix CentOS major and minor version setup from /etc/redhat-release
Now full, including year and month, release version is read from
/etc/redhat-release we should configure $releasemaj and $releasemin
exactly as we do for user supplied value via --releasever option.

Fixes: fc0fd36fc514 ("rhbootstrap: Include year and month components of CentOS 7 and 8 release")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -5250,8 +5250,9 @@ distro_centos()
             releasemaj="${releasever%-stream}"
             releasemin="$(centos_stream_compose_id)"
         else
-            releasemaj="${releasever%.*}"
-            releasemin="${releasever#*.}"
+            releasemaj="${releasever%%.*}"
+            releasemin="${releasever#$releasemaj.}"
+            releasemin="${releasemin%%.*}"
         fi
         releasemm="$releasemaj.$releasemin"
 