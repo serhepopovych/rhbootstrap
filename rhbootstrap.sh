serhepopovych/rhbootstrap

commit 0388d559810dbff6d22f68698f96d67c2b9d1a0d

rhbootstrap: Support CentOS Stream
It seems to be a future of CentOS and we need to start support it at
some point. Let's start doing this as early as possible.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -417,6 +417,8 @@ version_cmp()
 is_centos() { [ "${distro-}" = 'centos' ] || return; }
 is_fedora() { [ "${distro-}" = 'fedora' ] || return; }
 
+is_centos_stream() { is_centos && [ -z "${releasever%%*-stream}" ] || return; }
+
 # Usage: centos_version_cmp <v1> <v2>
 centos_version_cmp()
 {
@@ -476,6 +478,7 @@ distro_version()
     local root="${1-}"
 
     sed -n \
+        -e '1 s/^CentOS\s\+Stream\s\+.\+\s\+\([0-9]\+\).*$/\1-stream/p' \
         -e '1 s/^CentOS\s\+.\+\s\+\([0-9.]\+\).*$/\1/p' \
         -e '1 s/^Fedora\s\+.\+\s\+\([0-9]\+\).*$/\1/p' \
         "$root/etc/redhat-release" \
@@ -5206,8 +5209,13 @@ distro_centos()
         releasever="$(
             distro_version "$install_root"
         )"
-        releasemaj="${releasever%.*}"
-        releasemin="${releasever#*.}"
+        if [ -z "${releasever%%*-stream}" ]; then
+            releasemaj="${releasever%-stream}"
+            releasemin=163164162145141155 # stream -> %03o
+        else
+            releasemaj="${releasever%.*}"
+            releasemin="${releasever#*.}"
+        fi
         releasemm="$releasemaj.$releasemin"
 
         if [ -n "$is_archive" ]; then
@@ -5475,23 +5483,28 @@ distro_centos()
         releasemaj=8
         releasemin=${_releasemin}
     else
-        # There is some incompatibility with rpmdb(1)
-        # format that can't be addressed with rpmdb_dump/load
-        # helpers: install last supported and then update.
-        [ $releasever != '6.10' ] ||
-            releasever='6.9'
+        if [ -z "${releasever%%*-stream}" ]; then
+            releasemaj="${releasever%-stream}"
+            releasemin=163164162145141155 # stream -> %03o
+        else
+            # There is some incompatibility with rpmdb(1)
+            # format that can't be addressed with rpmdb_dump/load
+            # helpers: install last supported and then update.
+            [ $releasever != '6.10' ] ||
+                releasever='6.9'
 
-        releasemaj="${releasever%%.*}"
+            releasemaj="${releasever%%.*}"
+
+            if [ "$releasemaj" != "$releasever" ]; then
+                releasemin="${releasever#$releasemaj.}"
+                releasemin="${releasemin%%.*}"
+            else
+                releasemin=${_releasemin}
+            fi
+        fi
 
         [ $releasemaj -ge 4 ] ||
             fatal 'no support for CentOS before 4 (no yum?)'
-
-        if [ "$releasemaj" != "$releasever" ]; then
-            releasemin="${releasever#$releasemaj.}"
-            releasemin="${releasemin%%.*}"
-        else
-            releasemin=${_releasemin}
-        fi
     fi
 
     # $subdir
@@ -5761,7 +5774,7 @@ esac
 setarch "$basearch" \
 yum -y \
     ${releasemaj:+
-        --releasever=$releasemaj
+        --releasever=$releasever
      } \
     ${baseurl:+
         --disablerepo='*'
@@ -5909,6 +5922,10 @@ pkg_wireshark_gnome=1
 
   if is_centos; then
     if centos_version_gt $releasemaj 7; then
+        if is_centos_stream; then
+            pkg_wireshark_gnome=
+        fi
+
         # Disable packages that not available in repositories for CentOS/RHEL 8+
         pkg_iucode_tool=
 

commit c8b4231fefd5e9d90565356343cb016f689b5cd4

rhbootstrap: Use version comparison helpers where appropriate
Fixes: 0388d559810d ("rhbootstrap: Support CentOS Stream")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -427,6 +427,15 @@ version_cmp()
     return 3
 }
 
+version_lt() { [ "$(version_cmp "$@")" = '-1' ] || return; }
+version_eq() { [ "$(version_cmp "$@")" =  '0' ] || return; }
+version_gt() { [ "$(version_cmp "$@")" =  '1' ] || return; }
+
+version_le() { version_eq "$@" || return; }
+version_ge() { version_eq "$@" || return; }
+
+version_neq() { ! version_eq "$@" || return; }
+
 is_centos() { [ "${distro-}" = 'centos' ] || return; }
 is_fedora() { [ "${distro-}" = 'fedora' ] || return; }
 
@@ -5589,7 +5598,7 @@ distro_centos()
 
         # EPEL
         if [ -n "$repo_epel" ]; then
-            if [ $releasemaj -eq 8 -a $releasemin -lt 3 ]; then
+            if [ $releasemaj -eq 8 ] && version_lt $releasemm 8.3; then
                 # Enable PowerTools if EPEL is enabled to satisfy dependencies
                 in_chroot "$install_root" \
                     'yum config-manager --set-enabled PowerTools' \
@@ -5623,7 +5632,7 @@ distro_centos()
                     {} \+
                 fi
 
-                if [ $releasemaj -eq 4 -a $releasemin -le 3 ]; then
+                if [ $releasemaj -eq 4 ] && version_le $releasemm 4.3; then
                     # Backup /etc/yum.conf since yum(1) from EPEL doesn't have it
                     local t="$install_root/etc/yum.conf"
                     ln -nf "$t" "$t.rhbootstrap" ||: