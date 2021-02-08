serhepopovych/rhbootstrap

commit dfe788c5f00e16d17789deb3c17ccb0806bce611

rhbootstrap: Support for placing network interface (device) into group
It might be necessary to put specific network interface (device) into
device group. One of such use cases is reiptables firewall that uses
part of network device groups as trust zones.

This change only adds support for NetworkManager via ifcfg-rh plugin and
there is no such support for legacy network scripts.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -3230,6 +3230,25 @@ _EOF
 # Usage: config_network
 config_network()
 {
+    nm_devgroup()
+    {
+        local unpack_dir="$install_root"
+# md5(nm-devgroup.tgz.b64) = 75c59b9a7539bf673f6ea040f1ec9f63
+[ -d "$unpack_dir" ] || install -d "$unpack_dir"
+base64 -d -i <<'nm-devgroup.tgz.b64' | tar -zxf - -C "$unpack_dir"
+H4sIABhGB2AAA+3VW0/bMBQA4D77V5yllSKqpbnQJtJKeZk0tAdgmsQTmpBJTlqrxYlsZ+VS/vsc
+p0MIiQECMU0730Pj+nrs47Ro8vAIzbpSy0Mu+RxVWAhdc5MvUI2KsFYYNLUtRHFQ4M+5qpq690KR
+lWWZe1oPn64cj+1nkqZpMulFye54kvYgGY1es+zzNNpwBdBTVWX+1O+p9n8UPiP/RbWWr7kBL8t/
+1OY/y8aU//fwVP7f4vxd/ieTR/OfJWOX/zgd70Zx0oviLI1t/qM33Oej/vP89z9AeC5keM71grE+
+nGh7Bz7BoFbV/EzyC4Q9IQ2qkue4D3s8N6KS++wUBn0I5ggJ/IDNBvBSGIgYy7lG8AaJB0IysPzu
+D8Tf+L9/SfwdmE5d23BnO679YutQ87yN4ZAvEXSjENYIOV+tsACuQV9drIRc2pmhneumqT+2893a
+W2rjCa7tujdRvz8MB4mtGt5690KLbWgFhO1t11c6r2Qp5qHs7n2gcyVqo9kIRnd1ZSPdVjVjn4+P
+vnw9mHmD2GMSsTjrhtvluhaP6apROW7rGRMl2HhkG8/B9+OTb0EbyhTMArszOe1OaHZ3OF2gru/M
+L7Dkzcr4zPXFS8xB1OB2rtGAfRuhDQXcO2mLbpjHSsHYNgt/+04RQgghhBBCCCGEEEIIIYQQQggh
+5H39AlO38qQAKAAA
+nm-devgroup.tgz.b64
+    }
+
     nm_dnsmasq_split_0()
     {
         :
@@ -3338,6 +3357,13 @@ dnsmasq.tgz.b64
         for n in ${nameservers}; do
             echo "nameserver $n" >>"$t"
         done
+    fi
+
+    # Support for network device group in NetworkManager ifcfg-rh plugin
+    if [ -d "$install_root/etc/sysconfig/network-scripts" ]; then
+        if [ -n "${pkg_nm-}" ]; then
+            nm_devgroup
+        fi
     fi
 
     # Enable/disable legacy network scripts when no/ NetworkManager available

rootfs/etc/NetworkManager/dispatcher.d/01-devgroup
@@ -1,0 +1,27 @@
+#! /bin/bash
+
+# Usage: $prog_name <interface> <action>
+[ $# -ge 2 ] || exit 0
+
+case "$2" in
+    'pre-up'|'pre-down') ;;
+    *) exit 0    ;;
+esac
+
+# Make sure we called as symlink in pre-{up,down}.d/
+[ -z "${0##*/$2.d/*}" ] || exit 1
+
+cd /etc/sysconfig/network-scripts
+. ./network-functions
+
+CONFIG="$1"
+need_config "$CONFIG"
+source_config
+
+if [ -n "${GROUP-}" ]; then
+    [ "$2" = 'pre-up' ] || GROUP='default'
+
+    exec ip link set dev "$1" group "$GROUP"
+fi
+
+exit 0

rootfs/etc/NetworkManager/dispatcher.d/pre-down.d/01-devgroup
@@ -1,0 +1,1 @@
+../01-devgroup

rootfs/etc/NetworkManager/dispatcher.d/pre-up.d/01-devgroup
@@ -1,0 +1,1 @@
+../01-devgroup

rootfs/etc/NetworkManager/dispatcher.d/.bundle/nm-devgroup.conf
@@ -1,0 +1,3 @@
+../../../../etc/NetworkManager/dispatcher.d/pre-up.d/01-devgroup
+../../../../etc/NetworkManager/dispatcher.d/pre-down.d/01-devgroup
+../../../../etc/NetworkManager/dispatcher.d/01-devgroup

rootfs/etc/NetworkManager/dispatcher.d/.bundle/nm-devgroup.sh
@@ -1,0 +1,1 @@
+../../../../bundle.sh

commit d090413df50548468a1b5021625f0574ab7a40d9

rhbootstrap: Get rid of network-scripts specific code in devgroup hook
Fixes: dfe788c5f00e ("rhbootstrap: Support for placing network interface (device) into group")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -3252,19 +3252,20 @@ config_network()
     nm_devgroup()
     {
         local unpack_dir="$install_root"
-# md5(nm-devgroup.tgz.b64) = 76f86334122bf7ef2068cb90f1d35e59
+# md5(nm-devgroup.tgz.b64) = 0ab2661091f59a897181988fffe35fc6
 [ -d "$unpack_dir" ] || install -d "$unpack_dir"
 base64 -d -i <<'nm-devgroup.tgz.b64' | tar -zxf - -C "$unpack_dir"
-H4sIACFPB2AAA+3VW0/bMBQA4D77V5wlkSKqpbkQWmmlvEwa2gMwTeIJTcg4J63V4kS2s8Io/31O
-0qFtEgMEYpp2vpc4vh77OC1aER+jXVd6ecQVn6OOC2lqbsUC9aiIa41RU7tCkkYFfp3rqqkHT5Q4
-k8mkezq/P7tymidJOs7HWe76Zbv5OB1ANho9Z9nHaYzlGmCgq8r+qd9D7f8ofET+i2qtnnMDnp5/
-95JT/l/DQ/l/ifPv8r+3d2/+J1n+a/7TyXjXff/JC+7zXv95/v03EF9IFV9ws2DMh1Pj7sA7CGpd
-zc8Vv0TYl8qiLrnAA9jnwspKHbAzCHyI5ggZfIHNBvBKWkgYE9wgeEHmgVQMnLD/Awk34Y9fknAH
-ptOubbizHde+uDo0XLQxHPElgmk0whpB8NUKC+AGzPXlSqqlmxnauW6a+m073627pS6e6Jtb9ybx
-/WEcZK5qeOv9FFrqQisgbm+7uTaiUqWcx6q/95ERWtbWsBGM7urKRnVbNYy9Pzn+8PFw5gWpxxRi
-cd4Pd8v1LR4zVaMFbusZkyW4eFQbz+Hnk9NPURvKFOwC+zM5609odnc4faBd31lYYMmblQ1Z1xev
-UICsodu5QQvua4Q2FOi+SVfshnmslIxts/C37xQhhBBCCCGEEEIIIYQQQgghhBBCXtd3AZ7YcgAo
-AAA=
+H4sIAA3WIGAAA+2VXU/bMBSGe51fcZZGy0BL8wFppAG9mTS0C2DaxBVCk3FPWotgR7bDN/99TtpV
+LRKDDbQJ7Tw3sR2f9xzntR20PN5He6H06R6TbII6HgtTM8unqAfjuNYYNbVrJGk0xvOJVk3d+00S
+R1EU3dNx/9m1080kSYd5kifDXpJtbOZZD7LB4Dlpn0ZjLNMAPa2U/dW8x96/UvAJ/o/VhXzODvgD
+/4siJ///Bo/5/xLfv/M/zx/yP82SovU/TdI8G+YbvSQthmnRg+QF1/kg/7n//TcQnwgZnzAz9bw+
+HBq3Bz5AUGs1+S7ZGcK2kBZ1yTiOYJtxK5QceUcQ9CGaIGRwDLe3gJfCQuJ5nBkEP8h8ENIDRzj7
+gYS34c+bJFyDra3u3fraPK7tuDE0jLc17LFTBNNohAsEzqoKx8AMmKuzSshTpwyt1k1Tv2/17twu
+dfVE1y7vTdLvr8dB5obW7/yl0lLPEyUczUrbWVQFx1tgpzgr9ePB/qfPuzt+3J4Jc2W4kqWYxHJ2
+OiLDtaitiUXJy0kUpH4X1KlG2gnPwv0VyRY8ZxUE7xb9lj58U43m2K7FNCdmilUFVoFUFmpVVY1F
+QHkutJJnKO1KrMs4WEo3it35jGXjBLLR2/Re8kURfKrA3/16cPhlJwxuukZ0F/orE0ux6K55SwNu
+gbL9uPMof8VyrAx2s+biYyxZU9nQc8EeXiIHUUPnm0ELrlanlPrQ3Siu2UX53r8+BgRBEARBEARB
+EARBEARBEARBEARBEATxqvkBy+LWjAAoAAA=
 nm-devgroup.tgz.b64
     }
 

rootfs/etc/NetworkManager/dispatcher.d/01-devgroup
@@ -11,17 +11,19 @@ esac
 # Make sure we called as symlink in pre-{up,down}.d/
 [ -z "${0##*/$2.d/*}" ] || exit 1
 
-cd /etc/sysconfig/network-scripts
-. ./network-functions
-
-CONFIG="$1"
-need_config "$CONFIG"
-source_config
-
-if [ -n "${GROUP-}" ]; then
-    [ "$2" = 'pre-up' ] || GROUP='default'
-
-    exec ip link set dev "$1" group "$GROUP"
+if [ "$2" = 'pre-up' ]; then
+    CONFIG="/etc/sysconfig/network-scripts/ifcfg-$1"
+    if [ -r "$CONFIG" ]; then
+        eval $(
+            # Source in subshell to not pollute environment
+            if . "$CONFIG" >/dev/null 2>&1; then
+                echo "GROUP='${GROUP-}'"
+            fi
+        )
+    fi
+    [ -n "${GROUP-}" ] || exit 0
+else
+    GROUP='default'
 fi
 
-exit 0
+exec ip link set dev "$1" group "$GROUP"