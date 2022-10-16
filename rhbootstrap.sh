serhepopovych/rhbootstrap

commit 5a01ae13dd8fb71c82965a7322e322188c687ee0

rhbootstrap: Improve initramfs regeneration
Manually regenerate initramfs for all installed kernels to support old
systems with early dracut(8) that does not have --regenerate-all option.

Make sure /etc/fstab always exist to avoid execution errors in package
pre/post install hooks and dracut(8) scripts.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -6659,7 +6659,18 @@ _EOF
         # Update initramfs file
         if [ -n "${pkg_dracut-}" ]; then
             in_chroot "$install_root" '
-                dracut --force --regenerate-all
+                if dracut --help 2>&1 | grep -q -- "--regenerate-all"; then
+                    exec dracut --force --quiet --regenerate-all
+                else
+                    for kmod in /lib/modules/*; do
+                        if [ -d "$kmod" ] &&
+                           kver="${kmod##*/}" &&
+                           [ -n "$kver" -a -f "/boot/vmlinuz-$kver" ]
+                        then
+                            dracut --force --quiet "/boot/initramfs-$kver.img" "$kver"
+                        fi
+                    done
+                fi
             '
         fi
 
@@ -7537,6 +7548,13 @@ yum -y \
     install '@core' \
     ${PKGS-} \
     #
+
+f="${install_root}etc/fstab"
+if [ ! -e "$f" ]; then
+    # Remove broken symlink
+    rm -f "$f" ||:
+    : >"$f"
+fi
 
 if [ -n "${install_root%/}" ]; then
     # Convert rpmdb(1) from host to target format

commit 85e2329a1c44ef45203cbfb27feb15f46d9c108b

rhbootstrap: Drop quiet option unsupported by old dracut versions
Fixes: 5a01ae13dd8f ("rhbootstrap: Improve initramfs regeneration")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -4837,14 +4837,14 @@ add_dracutmodules+=" nfs "
         # Update initramfs file
         in_chroot "$install_root" '
             if dracut --help 2>&1 | grep -q -- "--regenerate-all"; then
-                exec dracut --force --quiet --regenerate-all
+                exec dracut --force --regenerate-all
             else
                 for kmod in /lib/modules/*; do
                     if [ -d "$kmod" ] &&
                        kver="${kmod##*/}" &&
                        [ -n "$kver" -a -f "/boot/vmlinuz-$kver" ]
                     then
-                        dracut --force --quiet "/boot/initramfs-$kver.img" "$kver"
+                        dracut --force "/boot/initramfs-$kver.img" "$kver"
                     fi
                 done
             fi