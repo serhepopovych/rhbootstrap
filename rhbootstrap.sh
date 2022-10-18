serhepopovych/rhbootstrap

commit 3d5dddfbb73ffca791ecdfe03cc0d071b32a01a3

rhbootstrap: Convert rpmdb from BerkeleyDB to SQLite on target systems
Since Fedora 34 RPM database changed from BerkeleyDB to SQLite so that
converting from host to target database format using rpmdb_dump and
rpmdb_load no longer works (rpmdb_load on target does not recognize
output produced by rpmdb_dump on host unless rpm backend is SQLite.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -7658,23 +7658,30 @@ fi
 
 if [ -n "${install_root%/}" ]; then
     # Convert rpmdb(1) from host to target format
-    {
-        t="${install_root}var/lib/rpm"
-        find "$t" ! -name 'Packages' -a -type f -a -exec rm -f {} \+
+    if rocky_version_ge $releasemaj 9 ||
+       centos_version_ge $releasemaj 9 ||
+       fedora_version_ge $releasemaj 34
+    then
+        in_chroot "$install_root" 'rpm --rebuilddb'
+    else
+        {
+            t="${install_root}var/lib/rpm"
+            find "$t" ! -name 'Packages' -a -type f -a -exec rm -f {} \+
 
-        t="$t/Packages" && f="$t.bak"
-        mv -f "$t" "$f"
+            t="$t/Packages" && f="$t.bak"
+            mv -f "$t" "$f"
 
-        /usr/lib/rpm/rpmdb_dump "$f"
+            /usr/lib/rpm/rpmdb_dump "$f"
 
-        rm -f "$f"
-    } | {
-        in_chroot_exec "$install_root" '
-            cd /var/lib/rpm &&
-            /usr/lib/rpm/rpmdb_load Packages &&
-            rpm --rebuilddb
-        '
-    }
+            rm -f "$f"
+        } | {
+            in_chroot_exec "$install_root" '
+                cd /var/lib/rpm &&
+                /usr/lib/rpm/rpmdb_load Packages &&
+                rpm --rebuilddb
+            '
+        }
+    fi
 fi
 
 # Perform distro specific actions post initial setup

commit be09b57946f44fc05e4ccf153cd9a97429da85a4

rhbootstrap: Also convert rpmdb from BerkeleyDB to SQLite on Fedora 33
Fixes: 3d5dddfbb73f ("rhbootstrap: Convert rpmdb from BerkeleyDB to SQLite on target systems")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -7688,7 +7688,7 @@ if [ -n "${install_root%/}" ]; then
     # Convert rpmdb(1) from host to target format
     if rocky_version_ge $releasemaj 9 ||
        centos_version_ge $releasemaj 9 ||
-       fedora_version_ge $releasemaj 34
+       fedora_version_ge $releasemaj 33
     then
         in_chroot "$install_root" 'rpm --rebuilddb'
     else