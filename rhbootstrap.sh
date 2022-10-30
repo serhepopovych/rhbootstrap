serhepopovych/rhbootstrap

commit c0ca3a10f87094c4ce4852bdd0d5078a1c381ff0

rhbootstrap: Improve NetworkManager package selection
For hypervisor setups using libvirt-kvm.*.conf profiles that enable
OpenvSwitch install NetworkManager-ovs if available.

Skip other NetworkManager packages for minimal desktop environment
profiles like xorg-mate and xorg-xfce.

Disable NetworkManager packages if they unavailable in base repositories
of selected distro and release. Use highest version that provide both
plugin and GNOME UI for it (e.g. NetworkManager-openvpn while available
in earlier versions enabled only since Fedora 24).

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -8231,6 +8231,7 @@ pkg_xorg_x11_utils=1
             pkg_dash=
             pkg_zsh_syntax_highlighting=
 
+            pkg_nm_strongswan=
             pkg_network_scripts=
 
             pkg_bluez_hid2hci=
@@ -8276,15 +8277,39 @@ pkg_xorg_x11_utils=1
         pkg_remmina_plugins_xdmcp=
 
         pkg_nm_vpnc=
-        pkg_nm_strongswan=
     else
-        if [ $releasemaj -eq 7 ]; then
-            :
-        else
-            pkg_cockpit=
-            pkg_ipxe_bootimgs=
-        fi
+        if version_lt $releasemm 7.5; then
+            if version_lt $releasemm 7.4; then
+                if version_lt $releasemm 7.1; then
+                    if [ $releasemaj -lt 7 ]; then
+                        if [ $releasemaj -lt 6 ]; then
+                            pkg_nm_modem=
+                        fi # < 6
+                        pkg_nm_tui=
+                        pkg_nm_libreswan=
+                        pkg_ipxe_bootimgs=
+                    fi # < 7
+                    pkg_nm_team=
+                    pkg_nm_adsl=
+                    pkg_nm_wifi=
+                    pkg_nm_bluez=
+                    pkg_nm_wwan=
+                fi # < 7.1
+                pkg_cockpit=
+            fi # < 7.4
+            pkg_nm_ovs=
+        fi # < 7.5
     fi
+
+    if [ -z "$has_epel" ]; then
+        pkg_nm_openvpn=
+        pkg_nm_openconnect=
+        pkg_nm_vpnc=
+        pkg_nm_strongswan=
+        pkg_nm_l2tp=
+        pkg_nm_pptp=
+    fi
+    pkg_nm_ssh=
 elif is_fedora; then
     ## <= $releasemaj
 
@@ -8309,15 +8334,34 @@ elif is_fedora; then
         pkg_mate=
     }
     fedora_le_18() {
+        pkg_nm_ssh=
         pkg_va_vdpau_driver=
     }
     fedora_le_19() {
+        pkg_nm_openvpn=
+        pkg_nm_pptp=
+        pkg_nm_vpnc=
         [ "${x11_server-}" != 'x2go' ] || x11_server='Xorg'
     }
+    fedora_le_20() {
+        pkg_nm_adsl=
+        pkg_nm_wifi=
+        pkg_nm_bluez=
+        pkg_nm_wwan=
+    }
+    fedora_le_21() {
+        pkg_nm_team=
+    }
     fedora_le_23() {
+        # gnome
+        pkg_nm_libreswan=
+        pkg_nm_strongswan=
         pkg_flatpak=
     }
     fedora_le_24() {
+        # gnome
+        pkg_nm_l2tp=
+        pkg_nm_openconnect=
         pkg_glvnd=
         pkg_chromium=
         pkg_pidgin_hangouts=
@@ -9616,7 +9660,7 @@ if [ -n "${pkg_nm-}" ]; then
 
         [ -z "${pkg_nm_libreswan-}" ] ||
             PKGS="$PKGS NetworkManager-libreswan-gnome"
-        [ -z "${pkg_nm_openswan-}" ] ||
+        [ -z "${pkg_nm_strongswan-}" ] ||
             PKGS="$PKGS NetworkManager-strongswan-gnome"
 
         [ -z "${pkg_nm_l2tp-}" ] ||

conf.d/all/libvirt-kvm.conf
@@ -422,12 +422,12 @@ pkg_libguestfs_winsupport=1
 pkg_nm=1
 
 # NetworkManager-tui
-pkg_nm_tui=
+pkg_nm_tui=1
 
 # NetworkManager-team
 pkg_nm_team=
 # NetworkManager-ovs
-pkg_nm_ovs=
+pkg_nm_ovs=1
 # NetworkManager-adsl
 pkg_nm_adsl=
 # NetworkManager-wifi
@@ -440,10 +440,12 @@ pkg_nm_wwan=
 pkg_nm_modem=
 
 # NetworkManager-openvpn
-pkg_nm_openvpn=1
+pkg_nm_openvpn=
+# NetworkManager-ssh
+pkg_nm_ssh=
+
 # NetworkManager-openconnect
 pkg_nm_openconnect=
-
 # NetworkManager-vpnc
 pkg_nm_vpnc=
 

conf.d/all/libvirt-kvm.xorg-desktop-mate.conf
@@ -669,12 +669,12 @@ pkg_bluez_hid2hci=1
 pkg_nm=1
 
 # NetworkManager-tui
-pkg_nm_tui=
+pkg_nm_tui=1
 
 # NetworkManager-team
-pkg_nm_team=
+pkg_nm_team=1
 # NetworkManager-ovs
-pkg_nm_ovs=
+pkg_nm_ovs=1
 # NetworkManager-adsl
 pkg_nm_adsl=1
 # NetworkManager-wifi
@@ -688,9 +688,11 @@ pkg_nm_modem=1
 
 # NetworkManager-openvpn
 pkg_nm_openvpn=1
+# NetworkManager-ssh
+pkg_nm_ssh=1
+
 # NetworkManager-openconnect
 pkg_nm_openconnect=1
-
 # NetworkManager-vpnc
 pkg_nm_vpnc=1
 

conf.d/all/libvirt-kvm.xorg-desktop-xfce.conf
@@ -696,12 +696,12 @@ pkg_bluez_hid2hci=1
 pkg_nm=1
 
 # NetworkManager-tui
-pkg_nm_tui=
+pkg_nm_tui=1
 
 # NetworkManager-team
-pkg_nm_team=
+pkg_nm_team=1
 # NetworkManager-ovs
-pkg_nm_ovs=
+pkg_nm_ovs=1
 # NetworkManager-adsl
 pkg_nm_adsl=1
 # NetworkManager-wifi
@@ -715,9 +715,11 @@ pkg_nm_modem=1
 
 # NetworkManager-openvpn
 pkg_nm_openvpn=1
+# NetworkManager-ssh
+pkg_nm_ssh=1
+
 # NetworkManager-openconnect
 pkg_nm_openconnect=1
-
 # NetworkManager-vpnc
 pkg_nm_vpnc=1
 

conf.d/all/libvirt-kvm.xorg-mate.conf
@@ -357,9 +357,9 @@ pkg_ntpdate=1
 pkg_dpdk=
 
 # wpa_supplicant
-pkg_wpa_supplicant=1
+pkg_wpa_supplicant=
 # usb_modeswitch
-pkg_usb_modeswitch=1
+pkg_usb_modeswitch=
 
 ## Firewall management utilities
 
@@ -666,40 +666,42 @@ pkg_bluez_hid2hci=1
 pkg_nm=1
 
 # NetworkManager-tui
-pkg_nm_tui=
+pkg_nm_tui=1
 
 # NetworkManager-team
 pkg_nm_team=
 # NetworkManager-ovs
-pkg_nm_ovs=
+pkg_nm_ovs=1
 # NetworkManager-adsl
-pkg_nm_adsl=1
+pkg_nm_adsl=
 # NetworkManager-wifi
-pkg_nm_wifi=1
+pkg_nm_wifi=
 # NetworkManager-bluetooth
-pkg_nm_bluez=1
+pkg_nm_bluez=
 # NetworkManager-wwan
-pkg_nm_wwan=1
+pkg_nm_wwan=
 # ModemManager
-pkg_nm_modem=1
+pkg_nm_modem=
 
 # NetworkManager-openvpn
-pkg_nm_openvpn=1
-# NetworkManager-openconnect
-pkg_nm_openconnect=1
+pkg_nm_openvpn=
+# NetworkManager-ssh
+pkg_nm_ssh=
 
+# NetworkManager-openconnect
+pkg_nm_openconnect=
 # NetworkManager-vpnc
-pkg_nm_vpnc=1
+pkg_nm_vpnc=
 
 # NetworkManager-libreswan
-pkg_nm_libreswan=1
+pkg_nm_libreswan=
 # NetworkManager-strongswan
-pkg_nm_strongswan=1
+pkg_nm_strongswan=
 
 # NetworkManager-l2tp
-pkg_nm_l2tp=1
+pkg_nm_l2tp=
 # NetworkManager-pptp
-pkg_nm_pptp=1
+pkg_nm_pptp=
 
 # network-scripts
 pkg_network_scripts=1

conf.d/all/libvirt-kvm.xorg-xfce.conf
@@ -357,9 +357,9 @@ pkg_ntpdate=1
 pkg_dpdk=
 
 # wpa_supplicant
-pkg_wpa_supplicant=1
+pkg_wpa_supplicant=
 # usb_modeswitch
-pkg_usb_modeswitch=1
+pkg_usb_modeswitch=
 
 ## Firewall management utilities
 
@@ -693,40 +693,42 @@ pkg_bluez_hid2hci=1
 pkg_nm=1
 
 # NetworkManager-tui
-pkg_nm_tui=
+pkg_nm_tui=1
 
 # NetworkManager-team
 pkg_nm_team=
 # NetworkManager-ovs
-pkg_nm_ovs=
+pkg_nm_ovs=1
 # NetworkManager-adsl
-pkg_nm_adsl=1
+pkg_nm_adsl=
 # NetworkManager-wifi
-pkg_nm_wifi=1
+pkg_nm_wifi=
 # NetworkManager-bluetooth
-pkg_nm_bluez=1
+pkg_nm_bluez=
 # NetworkManager-wwan
-pkg_nm_wwan=1
+pkg_nm_wwan=
 # ModemManager
-pkg_nm_modem=1
+pkg_nm_modem=
 
 # NetworkManager-openvpn
-pkg_nm_openvpn=1
-# NetworkManager-openconnect
-pkg_nm_openconnect=1
+pkg_nm_openvpn=
+# NetworkManager-ssh
+pkg_nm_ssh=
 
+# NetworkManager-openconnect
+pkg_nm_openconnect=
 # NetworkManager-vpnc
-pkg_nm_vpnc=1
+pkg_nm_vpnc=
 
 # NetworkManager-libreswan
-pkg_nm_libreswan=1
+pkg_nm_libreswan=
 # NetworkManager-strongswan
-pkg_nm_strongswan=1
+pkg_nm_strongswan=
 
 # NetworkManager-l2tp
-pkg_nm_l2tp=1
+pkg_nm_l2tp=
 # NetworkManager-pptp
-pkg_nm_pptp=1
+pkg_nm_pptp=
 
 # network-scripts
 pkg_network_scripts=1

conf.d/all/x2go-desktop-mate.conf
@@ -620,10 +620,10 @@ pkg_bluez_hid2hci=1
 pkg_nm=1
 
 # NetworkManager-tui
-pkg_nm_tui=
+pkg_nm_tui=1
 
 # NetworkManager-team
-pkg_nm_team=
+pkg_nm_team=1
 # NetworkManager-ovs
 pkg_nm_ovs=
 # NetworkManager-adsl
@@ -639,9 +639,11 @@ pkg_nm_modem=1
 
 # NetworkManager-openvpn
 pkg_nm_openvpn=1
+# NetworkManager-ssh
+pkg_nm_ssh=1
+
 # NetworkManager-openconnect
 pkg_nm_openconnect=1
-
 # NetworkManager-vpnc
 pkg_nm_vpnc=1
 

conf.d/all/x2go-desktop-xfce.conf
@@ -647,10 +647,10 @@ pkg_bluez_hid2hci=1
 pkg_nm=1
 
 # NetworkManager-tui
-pkg_nm_tui=
+pkg_nm_tui=1
 
 # NetworkManager-team
-pkg_nm_team=
+pkg_nm_team=1
 # NetworkManager-ovs
 pkg_nm_ovs=
 # NetworkManager-adsl
@@ -666,9 +666,11 @@ pkg_nm_modem=1
 
 # NetworkManager-openvpn
 pkg_nm_openvpn=1
+# NetworkManager-ssh
+pkg_nm_ssh=1
+
 # NetworkManager-openconnect
 pkg_nm_openconnect=1
-
 # NetworkManager-vpnc
 pkg_nm_vpnc=1
 

commit a307eb31b747e06ce4f5977ac8e581f5849d8fd5

rhbootstrap: Skip NetworkManager-ovs on Fedora earlier than 28
It is not available until Fedora 28 and yum(1) now has strict=True set
option causing installation failure.

Also remove duplicates of pkg_nm_l2tp= and pkg_nm_openconnect= for
Fedora 24.

Fixes: c0ca3a10f870 ("rhbootstrap: Improve NetworkManager package selection")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

rhbootstrap.sh
@@ -8378,8 +8378,6 @@ elif is_fedora; then
         pkg_glvnd=
         pkg_chromium=
         pkg_pidgin_hangouts=
-        pkg_nm_openconnect=
-        pkg_nm_l2tp=
     }
     fedora_le_25() {
         pkg_driverctl=
@@ -8392,6 +8390,7 @@ elif is_fedora; then
         pkg_va_intel_hybrid_driver=
     }
     fedora_le_27() {
+        pkg_nm_ovs=
         pkg_iucode_tool=
         pkg_remmina_plugins_secret=
     }